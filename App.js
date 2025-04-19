20e018fc4fcfef07d1918e0d03e191c0e498707a
carmen <marreola9@ivytech.edu>
2025-04-19 00:11:32 -0400

    init

--- App.js
+++ App.js
@@ -1,18 +1,28 @@
 import React, { useState } from "react";
-import { View, Button, Platform } from "react-native";
+import { View, Button, Platform, Text } from "react-native";
 import styles from "./styles";
 import DateTimePicker from "@react-native-community/datetimepicker";
 
 export default function App() {
   // useState to manage the date and time
   const [time, setTime] = useState(new Date());
-  const [showPicker, setShowPicker] = useState(false);
+  const [showPicker, setShowDate] = useState(false);
 
+  const [date, setDate] = useState(new Date());
+  const [showDate, setShowPicker] = useState(false);
   // make a function that when the datapicker is changed, it will detect if the platfom is IOS
   const onChange = (event, selectedDate) => {
     setShowPicker(Platform.OS === "ios");
     if (selectedDate) {
       setTime(selectedDate);
+    }
+  };
+
+  //same thing for date
+  const onChangeDate = (event, selectedDate) => {
+    setShowPicker(Platform.OS === "ios");
+    if (selectedDate) {
+      setDate(selectedDate);
     }
   };
 
@@ -26,6 +36,19 @@ export default function App() {
           is24Hour={true}
           display="default"
           onChange={onChange}
+        />
+      )}
+
+      <Text>Seleced Date: {date.toDateString()}</Text>
+
+      <Button title="Pick Date" onPress={() => setDatePicker(true)} />
+      {showDate && (
+        <DateTimePicker
+          value={date}
+          mode="date"
+          is24Hour={true}
+          display="default"
+          onChange={onChangeDate}
         />
       )}
     </View>
